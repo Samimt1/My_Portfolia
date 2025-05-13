f1a56fe496cac598e6f653ec752b7e772e15df59
sami <bikijoker18@gmail.com>
2025-05-13 20:07:01 +0300

    Initial commit with project files

---
+++ src/content_option.js
@@ -0,0 +1,189 @@
+const logotext = "SAMI";
+const meta = {
+  title: "Samuel Esubalew",
+  description:
+    "I’m Sami Software Engineer_ Full stack devloper,currently working in Ethiopia",
+};
+
+const introdata = {
+  title: "I’m Samuel Esubalew",
+  animated: {
+    first: "I love coding",
+    second: "I code cool websites",
+    third: "I develop mobile apps",
+    fourth: "I Software tester",
+  },
+  description:
+    "I am a software engineer with skills in full-stack web development. I have experience in front-end technologies such as HTML, CSS, and JavaScript, react along with frameworks like next.js. On the back end, I work with nest and Node.js. Additionally, I have expertise in mobile app development using Android Studio and Flutter.",
+  your_img_url:
+    "https://as2.ftcdn.net/v2/jpg/06/97/17/65/1000_F_697176551_pUMjAjjjMeIETnJDU7fictOdxQTvXDFf.jpg",
+};
+
+const dataabout = {
+  title: "About my self",
+  aboutme:
+    "I design and develop innovative software solutions that meet user needs and enhance functionality. My expertise spans various programming languages and frameworks, enabling me to create efficient, scalable applications. I am committed to continuous learning and collaboration, ensuring that I stay updated with industry trends and best practices.",
+};
+const worktimeline = [
+  {
+    jobtitle: "Website Develoment",
+    where: "Addis Ababa, Bole",
+    date: "Monday - Wednesday",
+  },
+  {
+    jobtitle: " Mobile App Development",
+    where: "Addis Ababa, Mexico",
+    date: "Thursday - Friday",
+  },
+  {
+    jobtitle: "Software testing",
+    where: "Addis Abebe, Megenagna",
+    date: "Saturday",
+  },
+];
+
+const skills = [
+  {
+    name: "Python",
+    value: 90,
+  },
+  {
+    name: "Java",
+    value: 85,
+  },
+  {
+    name: "Javascript",
+    value: 70,
+  },
+  {
+    name: "React",
+    value: 70,
+  },
+  {
+    name: "C++",
+    value: 85,
+  },
+];
+
+const services = [
+  {
+    title: "Website Development",
+    description:
+      "I have a strong foundation in HTML, CSS, and JavaScript, which I utilize to create engaging and user-friendly web applications. My expertise in frameworks like React and Vue enables me to develop dynamic, component-driven interfaces that enhance user experience. I have successfully worked on various projects, including e-commerce websites and hotel booking platforms, where I implemented features such as responsive design, seamless navigation, and interactive product displays. By leveraging the capabilities of these frameworks, I am able to deliver high-performance applications that not only meet the needs of businesses but also provide an intuitive experience for users.",
+  },
+  {
+    title: "Mobile App Development",
+    description:
+      "I specialize in mobile app development using frameworks like React Native and Flutter, allowing me to create cross-platform applications with seamless performance. My experience includes building e-commerce and hotel booking apps, where I focus on delivering intuitive user interfaces and efficient functionality. By leveraging these frameworks, I ensure that users have a smooth and engaging experience, regardless of the device they use.",
+  },
+  {
+    title: "Software Testing",
+    description:
+      "I am dedicated to ensuring the quality and reliability of applications through meticulous testing processes. I utilize various testing methodologies, including manual and automated testing, to identify bugs and ensure functionality meets specifications. My attention to detail and analytical skills enable me to provide valuable feedback to development teams, contributing to the overall improvement of software products. I am passionate about delivering high-quality user experiences and continuously improving testing practices.",
+  },
+];
+
+const dataportfolio = [
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description: "cross-word",
+    link: "https://github.com/Samimt1/cross-word.git",
+  },
+  {
+    img: "https://picsum.photos/400/800/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+
+  {
+    img: "https://picsum.photos/400/600/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/300/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/550/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+  {
+    img: "https://picsum.photos/400/700/?grayscale",
+    description:
+      "The wisdom of life consists in the elimination of non-essentials.",
+    link: "#",
+  },
+];
+
+const contactConfig = {
+  YOUR_EMAIL: "esubalew.samiye@gmail.com",
+  YOUR_FONE: "+251960344062",
+  description: "Contact me ",
+  // creat an emailjs.com account
+  // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+  YOUR_SERVICE_ID: "service_id",
+  YOUR_TEMPLATE_ID: "template_id",
+  YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+  github: "https://github.com/Samimt1",
+  facebook: "https://web.facebook.com/michu.esubalew",
+  linkedin: "https://www.linkedin.com/in/biki-joker-538468334/",
+  twitter: "https://x.com/SamiSa30721",
+};
+export {
+  meta,
+  dataabout,
+  dataportfolio,
+  worktimeline,
+  skills,
+  services,
+  introdata,
+  contactConfig,
+  socialprofils,
+  logotext,
+};
